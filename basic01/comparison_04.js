8d4bc223a60d46dba58e3012b84361a900ceef1f
Muhammad-Basit-Ali <kashifgkhan@gmail.com>
2025-06-09 11:46:02 +0000

    Stack and Heap

--- basic01/comparison_04.js
+++ basic01/comparison_04.js
@@ -6,10 +6,36 @@
 // console.log(null == 0);   false
 // console.log(null >= 0);   ture
 
-console.log(undefined == 0); false
-console.log(undefined > 0);  false
-console.log(undefined < 0);  false
+// console.log(undefined == 0); false
+// console.log(undefined > 0);  false
+// console.log(undefined < 0);  false
 
 //===
 
 console.log ("2" === 2);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//  Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = 'Kashifdotcom'
+
+let anothername = myYoutubename;
+anothername = "chaiaurcack";
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: 'user@google.com',
+    pkid: 'user@pkid'
+}
+
+let userTwo = userOne;
+
+userTwo.email = "kashif@pkid.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
